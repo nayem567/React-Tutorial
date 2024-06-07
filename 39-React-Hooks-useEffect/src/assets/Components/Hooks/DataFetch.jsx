320d613bbb560df3fdf0ada746e2f0db6c4ae6cd
nayem567 <inew2732@gmail.com>
2024-06-07 21:10:12 +0600

    useEffect Data Fetch API

--- 39-React-Hooks-useEffect/src/assets/Components/Hooks/DataFetch.jsx
+++ 39-React-Hooks-useEffect/src/assets/Components/Hooks/DataFetch.jsx
@@ -1,32 +1,48 @@
 // 30th May, 2024
+// 31th May, 2024
+import { ErrorMessage } from "formik";
 import { useState, useEffect } from "react";
 
+const loadingMessage = <p>Todos is Loading...</p>;
+
 const DataFetch = () => {
-  const [todos, setTodos] = (useState([]));
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setTodos(data);
-      });
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("fetching is not successful");
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          setTodos(data);
+          setIsLoading(false);
+          setError(null)
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsLoading(false);
+        });
+    }, 2000);
   }, []);
+
+  const todosElement = todos.map((todo) => {
+    // return <p key={todo.id}>{todo.title}</p>;
+    return <p key={todo.id}>{todo.title}</p>;
+  });
 
   return (
     <div>
       <h1>Todos</h1>
-      {todos.map((todo) => {
-        // return <p key={todo.id}>{todo.title}</p>;
-        return(
-          <div>
-             <h3 key={todo.id}>{todo.title}</h3>
-          <p>{todo.completed}</p>
-          </div>
-         
-        )
-      })}
+      {error && <p>{error}</p>}
+      {isLoading && loadingMessage}
+      {todosElement}
     </div>
   );
 };
